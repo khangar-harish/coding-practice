6c0ec45c5bdc3b247b47fed9198ed43cc245481c
khangar-harish <khanhash1992@gmail.com>
2023-09-28 17:26:50 +0100

    Arrays leetcode problems

--- src/com/hash/basic/PrintFactors.java
+++ src/com/hash/basic/PrintFactors.java
@@ -1,10 +1,23 @@
 package com.hash.basic;
 
+import java.util.Scanner;
+
 public class PrintFactors {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Please enter input string");
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		PrintFactors.printFactors(number);
 
+	}
+	
+	public static void printFactors(int number) {
+		for(int i=1; i<=number; i++) {
+			if(number%i == 0) {
+				System.out.print(i+ " ");
+			}
+		}
 	}
 
 }
